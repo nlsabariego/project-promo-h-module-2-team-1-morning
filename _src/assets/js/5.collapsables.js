04a48b5134aa6a3e8dc5eb7257f54b8682fb2bcf
anapayanruiz <anapayan22@gmail.com>
2019-11-22 18:32:24 +0100

    Update collapsable

--- _src/assets/js/5.collapsables.js
+++ _src/assets/js/5.collapsables.js
@@ -1,15 +1,27 @@
-"use strict";
+'use strict';
 
-const collapsableTriggers = document.querySelectorAll(
-  ".js-collapsable--trigger"
-);
+const collapsableTriggers = document.querySelectorAll('.js-collapsable--trigger');
 
 function updateCollapsable(event) {
   const currentCollapsable = event.currentTarget.parentElement;
 
-  currentCollapsable.classList.toggle("js-collapsable-open");
+  //Compruebo si está abierto
+  if (currentCollapsable.classList.contains('js-collapsable-open')) {
+    //Si está abierto, lo puedo cerrar
+    currentCollapsable.classList.remove('js-collapsable-open')
+
+  } else {
+    //Si está cerado:
+    //lo cierro todos
+    for (const item of collapsableTriggers) {
+      item.parentElement.classList.remove('js-collapsable-open');
+    }
+    //Abro el correcto
+    currentCollapsable.classList.add('js-collapsable-open');
+  }
+
 }
 
 for (const item of collapsableTriggers) {
-  item.addEventListener("click", updateCollapsable);
+  item.addEventListener('click', updateCollapsable);
 }
