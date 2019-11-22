f6f8e7e7b62e4546b2b127ec469da51d8aac66b7
Noelia Luque <n.luque91@gmail.com>
2019-11-22 12:23:00 +0100

    collapsable share

--- _src/assets/js/5.collapsables.js
+++ _src/assets/js/5.collapsables.js
@@ -1,11 +1,15 @@
-const collapsableTriggers = document.querySelectorAll('.js-collapsable--trigger');
+"use strict";
+
+const collapsableTriggers = document.querySelectorAll(
+  ".js-collapsable--trigger"
+);
 
 function updateCollapsable(event) {
   const currentCollapsable = event.currentTarget.parentElement;
 
-  currentCollapsable.classList.toggle('js-collapsable-open');
-};
+  currentCollapsable.classList.toggle("js-collapsable-open");
+}
 
 for (const item of collapsableTriggers) {
-  item.addEventListener('click', updateCollapsable);
+  item.addEventListener("click", updateCollapsable);
 }
