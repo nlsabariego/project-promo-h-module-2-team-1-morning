a026eb4260db45d3976da11c4cad956264f0eb01
Noelia Luque <n.luque91@gmail.com>
2019-11-25 12:24:37 +0100

    create doAll handler

--- _src/assets/js/2.desing.js
+++ _src/assets/js/2.desing.js
@@ -1,32 +1,42 @@
 //constantes para campos "diseña"
 
-const inputPalette1 = document.querySelector(".js-form-palette1");
-const inputPalette2 = document.querySelector(".js-form-palette2");
-const inputPalette3 = document.querySelector(".js-form-palette3");
-const card = document.querySelector(".js-card");
+'use strict';
+
+const inputPalette1 = document.querySelector('.js-form-palette1');
+const inputPalette2 = document.querySelector('.js-form-palette2');
+const inputPalette3 = document.querySelector('.js-form-palette3');
+const card = document.querySelector('.js-card');
 
 //funciones handler para pintar paletas
 
-function paintPalette1() {
-  card.classList.add("js-card-palette1");
-  card.classList.remove("js-card-palette2");
-  card.classList.remove("js-card-palette3");
-}
+// function paintPalette1() {
+//   card.classList.add('js-card-palette1');
+//   card.classList.remove('js-card-palette2');
+//   card.classList.remove('js-card-palette3');
+// }
 
-function paintPalette2() {
-  card.classList.add("js-card-palette2");
-  card.classList.remove("js-card-palette1");
-  card.classList.remove("js-card-palette3");
-}
+// function paintPalette2() {
+//   card.classList.add('js-card-palette2');
+//   card.classList.remove('js-card-palette1');
+//   card.classList.remove('js-card-palette3');
+// }
 
-function paintPalette3() {
-  card.classList.add("js-card-palette3");
-  card.classList.remove("js-card-palette2");
-  card.classList.remove("js-card-palette1");
+// function paintPalette3() {
+//   card.classList.add('js-card-palette3');
+//   card.classList.remove('js-card-palette2');
+//   card.classList.remove('js-card-palette1');
+// }
+
+function paintPalette() {
+  card.classList.remove('js-card-palette1');
+  card.classList.remove('js-card-palette2');
+  card.classList.remove('js-card-palette3');
+  const number = document.querySelector('.design__form--radio:checked').value;
+  card.classList.add('js-card-palette' + number);
 }
 
 //funciones listener para pintar paletas
 
-inputPalette1.addEventListener("click", paintPalette1);
-inputPalette2.addEventListener("click", paintPalette2);
-inputPalette3.addEventListener("click", paintPalette3);
+inputPalette1.addEventListener('click', doAll);
+inputPalette2.addEventListener('click', doAll);
+inputPalette3.addEventListener('click', doAll);
