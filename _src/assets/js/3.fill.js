34929d0f12a1847608844f6728b477044c16e220
crismmgg <cristinammgg@gmail.com>
2019-11-26 11:28:43 +0100

    add share button color and show twitter url

--- _src/assets/js/3.fill.js
+++ _src/assets/js/3.fill.js
@@ -1,22 +1,23 @@
-"use strict";
+'use strict';
+
 //NOMBRE Y PROFESIÓN
 
 //constantes para nombre y profesión
 
-const defaultName = "Nombre Apellido";
-const defaultJob = "Front-end developer";
-const cardNameElement = document.querySelector(".js-card-name");
-const inputNameElement = document.querySelector(".js-form-name");
-const cardJobElement = document.querySelector(".js-card-job");
-const inputJobElement = document.querySelector(".js-form-job");
+const defaultName = 'Nombre Apellido';
+const defaultJob = 'Front-end developer';
+const cardNameElement = document.querySelector('.js-card-name');
+const inputNameElement = document.querySelector('.js-form-name');
+const cardJobElement = document.querySelector('.js-card-job');
+const inputJobElement = document.querySelector('.js-form-job');
 
 //constante para el botón reset
 
-const resetButton = document.querySelector(".js-reset-button");
+const resetButton = document.querySelector('.js-reset-button');
 
 function resetButtonHandler() {
-  const formFill = document.querySelector("#js-form-fill");
-  const formDesign = document.querySelector("#js-form-design");
+  const formFill = document.querySelector('#js-form-fill');
+  const formDesign = document.querySelector('#js-form-design');
   formFill.reset();
   formDesign.reset();
   nameData();
@@ -25,25 +26,27 @@ function resetButtonHandler() {
 }
 
 //Función para cambiar de color el botón de share
-const shareButton = document.querySelector(".js-share-button");
+const shareButton = document.querySelector('.js-share-button');
+const form = document.querySelector('.js-form');
 function checkForm() {
   if (form.checkValidity() === true) {
-    shareButton.classList.remove("js-items--filter");
+    shareButton.classList.remove('js-button--filter');
+  } else {
+    shareButton.classList.add('js-button--filter');
   }
 }
 
 //Función handler que reparte
 
-const form = document.querySelector(".js-form");
-
 function doAll() {
+  paintPalette();
   nameData();
   jobData();
-  paintPalette();
+  formIcon();
   checkForm();
 }
 
-resetButton.addEventListener("click", resetButtonHandler);
+resetButton.addEventListener('click', resetButtonHandler);
 
 //función handler pintar nombre
 
@@ -68,29 +71,29 @@ function jobData() {
 
 //función listeners para pintar nombre y profesión
 
-inputNameElement.addEventListener("keyup", doAll);
-inputJobElement.addEventListener("keyup", doAll);
+inputNameElement.addEventListener('keyup', doAll);
+inputJobElement.addEventListener('keyup', doAll);
 
 //ICONOS
 
 //constantes para enlazar con los links de los iconos
 
-const inputLinkedinElement = document.querySelector(".js-form-linkedin");
-const linkedin = document.querySelector(".js-card-linkedin");
-const inputGithubElement = document.querySelector(".js-form-github");
-const github = document.querySelector(".js-card-github");
-const inputEmailElement = document.querySelector(".js-form-email");
-const email = document.querySelector(".js-card-email");
-const inputTelElement = document.querySelector(".js-form-tel");
-const tel = document.querySelector(".js-card-tel");
+const inputLinkedinElement = document.querySelector('.js-form-linkedin');
+const linkedin = document.querySelector('.js-card-linkedin');
+const inputGithubElement = document.querySelector('.js-form-github');
+const github = document.querySelector('.js-card-github');
+const inputEmailElement = document.querySelector('.js-form-email');
+const email = document.querySelector('.js-card-email');
+const inputTelElement = document.querySelector('.js-form-tel');
+const tel = document.querySelector('.js-card-tel');
 
 //constantes para añadir o quitar el filtro de los iconos
 
-const filter = document.querySelector(".js-items--filter");
-const iconLinkedin = document.querySelector(".js-linkedin");
-const iconGithub = document.querySelector(".js-github");
-const iconEmail = document.querySelector(".js-email");
-const iconTel = document.querySelector(".js-tel");
+const filter = document.querySelector('.js-items--filter');
+const iconLinkedin = document.querySelector('.js-linkedin');
+const iconGithub = document.querySelector('.js-github');
+const iconEmail = document.querySelector('.js-email');
+const iconTel = document.querySelector('.js-tel');
 
 //función handler para enlazar links iconos y añadir filtros color
 
@@ -108,33 +111,33 @@ function formIcon() {
 
   //condiciones para añadir-quitar filtros de color ¿podríamos separarlas???
   if (linkedinValue) {
-    iconLinkedin.classList.remove("js-items--filter");
+    iconLinkedin.classList.remove('js-items--filter');
   } else {
-    iconLinkedin.classList.add("js-items--filter");
+    iconLinkedin.classList.add('js-items--filter');
   }
 
   if (githubValue) {
-    iconGithub.classList.remove("js-items--filter");
+    iconGithub.classList.remove('js-items--filter');
   } else {
-    iconGithub.classList.add("js-items--filter");
+    iconGithub.classList.add('js-items--filter');
   }
 
   if (emailValue) {
-    iconEmail.classList.remove("js-items--filter");
+    iconEmail.classList.remove('js-items--filter');
   } else {
-    iconEmail.classList.add("js-items--filter");
+    iconEmail.classList.add('js-items--filter');
   }
 
   if (telValue) {
-    iconTel.classList.remove("js-items--filter");
+    iconTel.classList.remove('js-items--filter');
   } else {
-    iconTel.classList.add("js-items--filter");
+    iconTel.classList.add('js-items--filter');
   }
 }
 
 //funciones listener de los iconos
 
-inputLinkedinElement.addEventListener("keyup", formIcon);
-inputGithubElement.addEventListener("keyup", formIcon);
-inputEmailElement.addEventListener("keyup", formIcon);
-inputTelElement.addEventListener("keyup", formIcon);
+inputLinkedinElement.addEventListener('keyup', doAll);
+inputGithubElement.addEventListener('keyup', doAll);
+inputEmailElement.addEventListener('keyup', doAll);
+inputTelElement.addEventListener('keyup', doAll);
--- _src/assets/js/4.share.js
+++ _src/assets/js/4.share.js
@@ -1,1 +1,17 @@
-//AQUI IRÁ EL JS DE COMPARTE
+'use strict';
+
+//URL Y TWITTER
+
+//constantes
+const shareButtonOk = document.querySelector('.js-share-button');
+const twitterContainer = document.querySelector('.js-share--url');
+
+//función handler
+function showUrlTwitter() {
+  if (form.checkValidity() === true) {
+    twitterContainer.classList.remove('js-share--url');
+  }
+}
+
+//función listener
+shareButtonOk.addEventListener('click', showUrlTwitter);
