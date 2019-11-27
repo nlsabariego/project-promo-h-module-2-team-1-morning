b7e118e387185ad43b832cc97c0e2d4915b239b2
anapayanruiz <anapayan22@gmail.com>
2019-11-27 20:47:47 +0100

    Update localStorage.js

--- _src/assets/js/7.localStorage.js
+++ _src/assets/js/7.localStorage.js
@@ -1,21 +1,28 @@
 'use strict';
 
 const formContainer = document.querySelector('.js-form');
-const body = document.querySelector('body');
 
-const handleDataInput = function() {
+
+
+const handleDataInput = function () {
+  console.log('aqui tecleo');
+  console.log('aqui cambio de input')
+  //obtenemos solamente el input de tipo radio que está selecionado buscando por su nombre
+  //y para buscar con querySelector una propiedad es con :checked
+  const inputPalleteChecked = document.querySelector('input[name="palette"]:checked');
   const data = {
     name: inputNameElement.value,
     job: inputJobElement.value,
     linkedin: inputLinkedinElement.value,
     github: inputGithubElement.value,
     email: inputEmailElement.value,
-    tel: inputTelElement.value
+    tel: inputTelElement.value,
+    palette: inputPalleteChecked.value,
   };
   localStorage.setItem('userData', JSON.stringify(data));
 };
 
-const getFromLocalStorage = function() {
+const getFromLocalStorage = function () {
   const userData = JSON.parse(localStorage.getItem('userData'));
   if (userData !== null) {
     inputNameElement.value = userData.name;
@@ -24,10 +31,18 @@ const getFromLocalStorage = function() {
     inputGithubElement.value = userData.github;
     inputEmailElement.value = userData.email;
     inputTelElement.value = userData.tel;
+    //buscamos el radio que tiene el mismo valor que hemos guardado en la localStorage y
+    //lo checkeamos (usamos checked como propiedad)
+    document.querySelector(`input[name="palette"][value="${userData.palette}"]`).checked = true;
   }
 };
 
-body.addEventListener('keyup', handleDataInput);
+//duda Miguel?
+//entramos en el evento cuando levantamos la tecla
+window.addEventListener('keyup', handleDataInput);
+//entramos en el evento cuando cambiamos de campo por lo que los cambios
+//solo se guardan en la caché cuando cambiamos de input
+window.addEventListener('change', handleDataInput);
 
 getFromLocalStorage();
 doAll();
