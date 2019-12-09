d548999d381ff0b530d08f25f7b3fa6baa82eaf1
sandrusmb <sandrusmb@gmail.com>
2019-12-09 18:38:46 +0100

    Add card url to twitter

--- _src/assets/js/5.share.js
+++ _src/assets/js/5.share.js
@@ -1,97 +1,96 @@
 /* eslint-disable no-unused-vars */
-'use strict';
+"use strict";
 
 //constantes
 
-const shareButtonOk = document.querySelector('.js-share-button');
-const twitterContainer = document.querySelector('.js-share--url');
-let cardUrl = '';
-
+const shareButtonOk = document.querySelector(".js-share-button");
+const twitterContainer = document.querySelector(".js-share--url");
+let cardUrl = "";
 
 // CAMBIAR COLOR BOTÓN SHARE
 
 //Función para cambiar de color el botón de share
 
-const shareButton = document.querySelector('.js-share-button');
-const form = document.querySelector('.js-form');
+const shareButton = document.querySelector(".js-share-button");
+const form = document.querySelector(".js-form");
 
 function checkForm() {
   if (form.checkValidity() === true) {
-    shareButton.classList.remove('js-button--filter');
-    shareButton.removeAttribute('disabled');
+    shareButton.classList.remove("js-button--filter");
+    shareButton.removeAttribute("disabled");
   } else {
-    shareButton.classList.add('js-button--filter');
-    twitterContainer.classList.add('js-share--url');
+    shareButton.classList.add("js-button--filter");
+    twitterContainer.classList.add("js-share--url");
   }
 }
 
 // URL Y TWITTER
 
-
 //función handler para desplegar url y botón compartir en twitter
 
 function showTwitter() {
   if (form.checkValidity() === true) {
-    twitterContainer.classList.remove('js-share--url');
+    twitterContainer.classList.remove("js-share--url");
   } else {
-    twitterContainer.classList.add('js-share--url');
-    alert('Ey! Faltan info, revisa tus datos.');
+    twitterContainer.classList.add("js-share--url");
+    alert("Ey! Faltan info, revisa tus datos.");
   }
   sendRequest(getDataObj());
 }
 
 //función listener botón naranja
 
-shareButtonOk.addEventListener('click', showTwitter);
+shareButtonOk.addEventListener("click", showTwitter);
 
 // Crear url de la card
 
 function sendRequest(getDataObj) {
   console.log(getDataObj);
-  fetch('https://us-central1-awesome-cards-cf6f0.cloudfunctions.net/card/', {
-      method: 'POST',
-      body: JSON.stringify(getDataObj),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-    .then(function (resp) {
+  fetch("https://us-central1-awesome-cards-cf6f0.cloudfunctions.net/card/", {
+    method: "POST",
+    body: JSON.stringify(getDataObj),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+    .then(function(resp) {
       return resp.json();
     })
-    .then(function (result) {
+    .then(function(result) {
       showURL(result);
     })
-    .catch(function (error) {
+    .catch(function(error) {
       console.log(error);
     });
 }
 
-const responseURL = document.querySelector('.js-notification-link');
+const responseURL = document.querySelector(".js-notification-link");
 
 function showURL(result) {
   if (result.success) {
     responseURL.innerHTML =
-      '<a href=' + result.cardURL + '>' + result.cardURL + '</a>';
+      "<a href=" + result.cardURL + ">" + result.cardURL + "</a>";
+    cardUrl = result.cardURL;
   } else {
-    responseURL.innerHTML = 'ERROR:' + result.error;
+    responseURL.innerHTML = "ERROR:" + result.error;
   }
 }
 
 // Botón twitter, crear tweet
 
-const buttonTwitter = document.querySelector('.js__button-Twitter');
+const buttonTwitter = document.querySelector(".js__button-Twitter");
 
 function openTwitter() {
   const userUrl = cardUrl;
-  const twitterText = '¡Hola! Esta es mi tarjeta de contacto. Saludos!';
-  const hashtags = 'adalab,CardToGo,promoHamilton,thereisnoplanetb';
+  const twitterText = "¡Hola! Esta es mi tarjeta de contacto. Saludos!";
+  const hashtags = "adalab,CardToGo,promoHamilton,thereisnoplanetb";
   buttonTwitter.href =
-    'https://twitter.com/intent/tweet?url=' +
+    "https://twitter.com/intent/tweet?url=" +
     encodeURIComponent(userUrl) +
-    '&text=' +
+    "&text=" +
     encodeURIComponent(twitterText) +
-    '&hashtags=' +
+    "&hashtags=" +
     encodeURIComponent(hashtags);
 }
 
-buttonTwitter.addEventListener('click', openTwitter);
+buttonTwitter.addEventListener("click", openTwitter);
